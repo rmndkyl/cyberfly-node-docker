59b036c35aabd3b247e23f80be5d55026202243b
Mandaa <99118244+rmndkyl@users.noreply.github.com>
2024-12-24 00:31:23 +0700

    Update restart_node.sh

--- restart_node.sh
+++ restart_node.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo docker compose -f updated-docker-compose.yaml down
-sudo docker compose -f updated-docker-compose.yaml up --force-recreate -d 
+sudo docker-compose -f updated-docker-compose.yaml down
+sudo docker-compose -f updated-docker-compose.yaml up --force-recreate -d 
